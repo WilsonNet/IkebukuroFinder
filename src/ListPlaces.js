2bf72a26e94e83811563855c8f3f61a05a41a598
Wilson Neto <wilsonpsn@hotmail.com>
2019-03-30 13:34:23 -0300

    Added Places.json file

---
+++ src/ListPlaces.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ListPlaces extends Component {
+
+    render() {
+        return(
+            <ul>
+                <li></li>
+            </ul>
+        )
+    }
+}
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -13,12 +13,11 @@ class MapContainer extends Component {
     
     initMap = () => {
         // The location of Uluru
-         
         const ikebukuro = { lat: 35.724663768, lng: 139.70666384 };
         // The map, centered at Uluru
         const map = new window.google.maps.Map(document.getElementById('map'), { zoom: 12, center: ikebukuro });
         // The marker, positioned at Uluru
-        const marker = new window.google.maps.Marker({ position: ikebukuro, map: map });
+        const marker = new window.google.maps.Marker({ title: ikebukuro, position: ikebukuro, map: map });
     }
     componentDidMount() {
         window.initMap = this.initMap;
