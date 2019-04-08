ffb652aa5a8e842064eab0d03f98037d17f1e2cf
2017102446 <2017102446@aluno.unicuritiba.edu.br>
2019-04-08 19:44:04 -0300

    Testing git

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import MainScreen from './MainScreen';
+//Oi
 
 class App extends Component {
   render() {
